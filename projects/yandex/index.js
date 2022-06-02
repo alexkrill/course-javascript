91d5c19a76b21ed8e247f9b593e1e810ae19a2c6
Alexey Lamkin <onewayroad@mail.ru>
2022-06-02 19:32:35 +0300

    clasterisation fix and local storage added

--- projects/yandex/index.js
+++ projects/yandex/index.js
@@ -1,8 +1,7 @@
 import './yandex.html'
 import { formTemplate } from './templates';
 
-const reviews = []
-
+let clusterer;
 
 document.addEventListener('DOMContentLoaded', () => {
     ymaps.ready(init);
@@ -13,53 +12,50 @@ document.addEventListener('DOMContentLoaded', () => {
             zoom: 13
         })
 
-        myMap.events.add('click', function (e) {
+        myMap.events.add('click', async function (e) {
             const coords = e.get('coords');
 
-            openBalloon(myMap, coords)
+            openBalloon(myMap, coords, [])
+        })
+
+        clusterer = new ymaps.Clusterer({ clusterDisableClickZoom: true });
+        clusterer.options.set('hasBalloon', false);
+
+        getGeoObjects(myMap)
+        clusterer.events.add('click', function(e) {
+          let geoObjectsInCluster = e.get('target').getGeoObjects()
+          openBalloon(myMap, e.get('coords'), geoObjectsInCluster)
         })
     }
 })
 
-function getOptionsCluster(coords) {
-    const clusterObjects = [];
+function getReviewsFromLS() {
+    const reviews = localStorage.reviews
+    return JSON.parse(reviews || "[]")
+  }
 
-    for (const review of reviews) {
-        if (JSON.stringify(review.coords) === JSON.stringify(coords)){
-            const geoObj = new ymaps.GeoObject({
-                geometry: {type: 'Point', coordinates: coords}
-            })
-            clusterObjects.push(geoObj);
-        }
-        
+function getGeoObjects(map) {
+    const geoObjects = []
+    for (const review of getReviewsFromLS() || []) {
+      const placemark = new ymaps.Placemark(review.coords);
+      placemark.events.add('click', e => {
+        e.stopPropagation();
+        openBalloon(map, e.get('coords'), [e.get('target')])
+      })
+      geoObjects.push(placemark);
     }
-    return clusterObjects;
-}
+  
+    clusterer.removeAll()
+    map.geoObjects.remove(clusterer)
+    clusterer.add(geoObjects)
+    map.geoObjects.add(clusterer)
+  }
 
-function addCluster(map, coords) {
-    const clusterer = new ymaps.Clusterer({clusterDisableClickZoom: true});
-    clusterer.options.set('hasBalloon', false);
-
-    function addToCluster() {
-        const myGeoObjects = getOptionsCluster(coords);
-        clusterer.add(myGeoObjects);
-        map.geoObjects.add(clusterer);
-        map.balloon.close()
-    }
-
-    clusterer.events.add('click', function (e) {
-        e.preventDefault();
-        openBalloon(map, coords, clusterer, addToCluster)
-    })
-
-    addToCluster();
-}
-
-function getReviewList(coords) {
+function getReviewList(currentGeoObjects) {
     let reviewListHTML = '';
 
-    for (const review of reviews) {
-        if (JSON.stringify(review.coords) === JSON.stringify(coords)) {
+    for (const review of getReviewsFromLS()) {
+        if (currentGeoObjects.some(geoObject => JSON.stringify(geoObject.geometry._coordinates) === JSON.stringify(review.coords))) {
             reviewListHTML += `
             <div class="user-review">
                 <div class="user-info">
@@ -76,25 +72,26 @@ function getReviewList(coords) {
     return reviewListHTML;
 }
 
-async function openBalloon(map, coords, clusterer, fn) {
+async function openBalloon(map, coords, currentGeoObjects) {
     await map.balloon.open(coords, {
-        content: `<div class="reviews">${getReviewList(coords)}</div>${formTemplate}`
+        content: `<div class="reviews">${getReviewList(currentGeoObjects)}</div>${formTemplate}`
     })
 
     document.querySelector('#add-form').addEventListener('submit', function(e) {
         e.preventDefault();
-        if (clusterer) {
-            clusterer.removeAll();
-        }
-
-        reviews.push({
-            coords: coords,
+        const review = {
+            coords,
             author: this.elements.author.value,
             place: this.elements.place.value,
             reviewText: this.elements.review.value,
-        })
-        !fn ? addCluster(map, coords) : fn()
-        map.balloon.close()
+        };
+
+        localStorage.reviews = JSON.stringify([...getReviewsFromLS(), review])
+
+        getGeoObjects(map)
+    
+        map.balloon.close();
+    
     })
 }
 
